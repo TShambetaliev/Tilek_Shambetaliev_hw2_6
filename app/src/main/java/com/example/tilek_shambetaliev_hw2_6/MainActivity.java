f289f516d755c17f8037ca1e7e8adc440291b40d
Tshambetaliev <tshambetaliev@gmail.com>
2023-01-24 15:55:02 +0600

    toest1

--- app/src/main/java/com/example/tilek_shambetaliev_hw2_6/MainActivity.java
+++ app/src/main/java/com/example/tilek_shambetaliev_hw2_6/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -25,11 +27,59 @@ public class MainActivity extends AppCompatActivity {
         TextView parolnajat = findViewById(R.id.najmite);
         TextView vypolnitevhod = findViewById(R.id.vypolnite_vhod);
         TextView vhod = findViewById(R.id.vhod);
+        /*if (password.getText().toString().equals(null)&&login.getText().toString().equals(null)){
+            voyti.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.gray));
+        } else {
+            voyti.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.orange));
+        }*/
+        login.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence cS1, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence cS1, int start, int before, int count) {
+                if (login.getText().toString().isEmpty()){
+                    voyti.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.gray));
+                } else {
+                    voyti.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.orange));
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence cS1, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence cS1, int start, int before, int count) {
+                if (password.getText().toString().isEmpty()) {
+                    voyti.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.gray));
+                } else {
+                    voyti.setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.orange));
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         findViewById(R.id.button_vhod).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (password.getText().toString().equals("admin") || login.getText().toString().equals("admin")){
+                if (password.getText().toString().equals("admin") && login.getText().toString().equals("admins")){
                     password.animate().alpha(0).setDuration(1000);
                     login.animate().alpha(0).setDuration(1000);
                     voyti.animate().alpha(0).setDuration(1000);
@@ -44,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                 }
             }
+
         });
+
     }
     }
